e7160cd8832553bae88c232182933a6c209a0e86
성균 정 <tacoyaggi93@triptopaz.com>
2025-04-02 09:15:29 +0900

    동기화

---
+++ Test/TestConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+string text = Console.ReadLine();
+
+string[] arryString = text.Split(" ");
+
+int one = Convert.ToInt32(arryString[0]);
+int two = Convert.ToInt32(arryString[1]);
+
+Console.WriteLine(one / two);
+Console.WriteLine(one % two);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
