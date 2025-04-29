0a06dad359380c34cda7aaf5fb1b69a563c1b9c5
성균 정 <tacoyaggi93@triptopaz.com>
2025-04-29 16:04:50 +0900

    OOP

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -26,6 +26,7 @@ public class MemoryMan
 
     #endregion
 
+    #region 캡슐화
     public class BankService
     {
         private int money = 10000; // 캡슐화
@@ -49,11 +50,85 @@ public class MemoryMan
         }
     }
 
+    #endregion
+
+    #region 상속
+
+    public class Parent
+    {
+        public void Money()
+        {
+            Console.WriteLine("돈");
+        }
+    }
+
+    public class Son : Parent
+    {
+        public void Play()
+        {
+            Console.WriteLine("놀다");
+        }
+    }
+
+    public class Me
+    {
+        public void Study()
+        {
+            Console.WriteLine("공부");
+        }
+    }
+
+    #endregion
+
+    #region 다형성
+
+    #region 오버로딩
+    public void Over(int a)
+    {
+        Console.WriteLine("오버로딩1");
+    }
+
+    public void Over(int a, int b)
+    {
+        Console.WriteLine("오버로딩2");
+    }
+    #endregion
+
+    #region 오버라이딩
+    public class OverParent
+    {
+        public virtual void OverMoney()
+        {
+            Console.WriteLine("부모 돈");
+        }
+    }
+
+    public class OverSon : OverParent
+    {
+        public override void OverMoney()
+        {
+            Console.WriteLine("자식 돈");
+        }
+    }
+
+    public class OverSon2 : OverParent
+    {
+        //public override void OverMoney()
+        //{
+        //    Console.WriteLine("자식2 돈");
+        //}
+    }
+
+    #endregion
+
+    #endregion
+
     public static void Main()
     {
-        BankService service = new BankService();
-        int price = service.Withdraw(5000);
-        service.ChangeMoney(price);
+        OverParent son = new OverSon();
+        OverParent son2 = new OverSon2();
+        son.OverMoney();
+        son2.OverMoney();
     }
 }
 
