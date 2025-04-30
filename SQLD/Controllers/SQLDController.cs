2f845427ce2761d80e6e04d13bfce0b7e7cc27ec
성균 정 <tacoyaggi93@triptopaz.com>
2025-04-30 18:17:33 +0900

    SQLD

---
+++ SQLD/Controllers/SQLDController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SQLD.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class SQLDController : ControllerBase
+    {
+        public async Task<IActionResult> Get()
+        {
+
+            return Ok();
+        }
+    }
+}
