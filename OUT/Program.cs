8cda303d7fd0a86e27b2fb46fcf867bae224e6a4
성균 정 <tacoyaggi93@triptopaz.com>
2025-05-09 16:21:20 +0900

    out 추가

---
+++ OUT/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+public class MemoryMan
+{
+    internal int Test(int width, out int height)
+    {
+        height = width;
+
+        return width;
+    }
+
+    int RefTest(int width, ref int height)
+    {
+        return width;
+    }
+
+    internal static void Main()
+    {
+        #region out 기본형
+        MemoryMan memory = new MemoryMan();
+        var result = memory.Test(500, out int h);
+        Console.WriteLine("width : {0}", result);
+        Console.WriteLine("height : {0}", h);
+        #endregion
+
+        #region TryParse
+
+        var tryParseTest = int.TryParse("123", out int tryParseResult);
+
+        #endregion
+
+        #region ref
+        var paramRef = 200;
+        memory.RefTest(100, ref paramRef);
+        #endregion
+    }
+
+    class Dto
+    {
+        public int Seq { get; set; }
+        public string Name { get; set; }
+
+        public long MaxValue { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
--- Test/TestConsole/Program.cs
+++ Test/TestConsole/Program.cs
@@ -1,1 +1,67 @@
 ﻿
+
+
+#region 1번문제
+
+class Result
+{
+
+    /*
+     * Complete the 'finalState' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY operations as parameter.
+     */
+
+    public static long finalState(List<List<int>> operations)
+    {
+        int maxIndex = 100001;
+        int[] diff = new int[maxIndex + 2];
+
+        foreach (var item in operations)
+        {
+            int first = item[0];
+            int second = item[1];
+            diff[first] += 1;
+            diff[second + 1] -= 1;
+        }
+
+        long sum = 0;
+        int current = 0;
+
+
+        for (int i = 1; i <= 100000; i++)
+        {
+            current += diff[i];
+            if (Math.Abs(current) % 2 == 1)
+                sum += i;
+        }
+
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main()
+    {
+        int operationsRows = Convert.ToInt32(Console.ReadLine().Trim());
+        int operationsColumns = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> operations = new List<List<int>>();
+
+        for (int i = 0; i < operationsRows; i++)
+        {
+            operations.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(operationsTemp => Convert.ToInt32(operationsTemp)).ToList());
+        }
+
+        long result = Result.finalState(operations);
+
+        Console.WriteLine(result);
+
+    }
+}
+
+
+#endregion
